1a3a0442da28d0bb662aaaab9ad8f086874fad54
Bert Deferme <bert@bdeferme.net>
2019-06-11 14:13:29 +0200

    Update repo.pp for puppet6

--- manifests/puppet/repo.pp
+++ manifests/puppet/repo.pp
@@ -1,19 +1,19 @@
 class puppet::puppet::repo {
 
   if $facts['virtual'] == 'kvm' {
-    $baseurl = 'file:///vagrant/repos/puppet5/el/7/x86_64'
+    $baseurl = 'file:///vagrant/repos/puppet6/el/7/x86_64'
   } else {
     if $facts['puppet_master_env'] {
-      $baseurl = "http://your.local.mirror/repo/puppet5_${facts['puppet_master_env']}/el/7/x86_64/"
+      $baseurl = "http://your.local.mirror/repo/puppet6_${facts['puppet_master_env']}/el/7/x86_64/"
     } else {
-      $baseurl = 'http://yum.puppet.com/puppet5/el/7/x86_64/'
+      $baseurl = 'http://yum.puppet.com/puppet6/el/7/x86_64/'
     }
   }
 
-  yumrepo { 'puppet5':
+  yumrepo { 'puppet6':
     ensure   => present,
     baseurl  => $baseurl,
-    descr    => 'Puppet5 packages',
+    descr    => 'Puppet6 packages',
     enabled  => '1',
     gpgcheck => '0',
   }
