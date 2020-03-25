5d3f3462a0451a0ba0d849413236afe41528d627
Johan De Wit <johan@koewacht.net>
2020-03-25 21:28:13 +0100

    correct repo settings for puppet version

--- manifests/puppet/repo.pp
+++ manifests/puppet/repo.pp
@@ -5,7 +5,7 @@ class puppet::puppet::repo (
   if ! $puppet_version in [ 5, 6 ] {
     fail("Only puppet 5 or puppet 6 version is supported, got puppet${puppet_version}")
   }
-  
+
   case $facts['os']['name'] {
     'RedHat','CentOS': {
       $os_flavor = 'el'
@@ -17,7 +17,7 @@ class puppet::puppet::repo (
       fail("Only RedHat family based distros are supported: got ${os['name']}")
     }
   }
- 
+
   file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet${puppet_version}-release":
     ensure => present,
     source => 'https://yum.puppetlabs.com/RPM-GPG-KEY-puppet',
@@ -26,10 +26,10 @@ class puppet::puppet::repo (
   yumrepo { "puppet${puppet_version}":
     ensure   => present,
     baseurl  => "https://yum.puppetlabs.com/puppet${puppet_version}/${os_flavor}/${facts['os']['release']['major']}/${facts['os']['architecture']}",
-    descr    => "Puppet 6 Repository ${os_flavor} ${facts['os']['release']['major']} - ${facts['os']['architecture']}",
+    descr    => "Puppet ${puppet_version}  Repository ${os_flavor} ${facts['os']['release']['major']} - ${facts['os']['architecture']}",
     enabled  => '1',
     gpgcheck => '1',
-    gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet6-release', 
+    gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet${puppet_version}-release",
     require  => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet${puppet_version}-release"],
   }
 }
